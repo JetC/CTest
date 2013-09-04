5f7275aa86b533165ad551cd9f8d18f366b8427a
009988765432 <g7733pf@gmail.com>
2013-09-04 18:29:00 +0800

    CreateVS2012

---
+++ CTest/CTest.cpp
@@ -0,0 +1,14 @@
+
+#include "stdafx.h"
+#include "stdio.h"
+#include "stdlib.h"
+int main(int argc, char const *argv[])
+{
+	float a,b,c,v;
+	a=2.0;
+	b=c=3.0;
+	v=a*b*c;
+	printf("%f\n", v);
+	system("pause");
+	return 0;
+}
