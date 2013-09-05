5577a2fadc2b2c12b1802124afd9679fb0c91ef0
009988765432 <g7733pf@gmail.com>
2013-09-05 17:00:16 +0800

    0905

--- CTest/CTest.cpp
+++ CTest/CTest.cpp
@@ -4,11 +4,32 @@
 #include "stdlib.h"
 int main(int argc, char const *argv[])
 {
-	float a,b,c,v;
+	float a,b,c,d;
+	int max1(int a,int b);	//在main函数中要声明其他函数
+	int max2(int a,int b);
 	a=2.0;
-	b=c=3.0;
-	v=a*b*c;
-	printf("%f\n", v);
+	b=3.0;
+	c=max1(a,b);
+	d=max2(a,b);
+	printf("%f\n", c);
+	printf("%f\n", d);
 	system("pause");
 	return 0;
+}
+
+int max1(int a,int b)
+{
+	int c;
+	if (a<b)
+		c=b;
+	else
+		c=a;
+	return c;
+}
+
+int max2(int a,int b)
+{
+	int c;
+	a<b?c=b:c=a;
+	return c;
 }
