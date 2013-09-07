2d2fcc65215f0f060f9d17e55ee8e6033e0d1650
009988765432 <g7733pf@gmail.com>
2013-09-07 16:57:50 +0800

    16:57

--- CTest/CTest.cpp
+++ CTest/CTest.cpp
@@ -4,20 +4,36 @@
 #include "stdlib.h"
 int main(int argc, char const *argv[])
 {
-	float a,b,c,d;
-	int max1(int a,int b);	//在main函数中要声明其他函数
-	int max2(int a,int b);
-	a=2.0;
-	b=3.0;
-	c=max1(a,b);
-	d=max2(a,b);
+	float a,b,c,d,e,tmp;
+	int max(int a,int b);
+	int min(int a,int b);	//在main函数中要声明其他函数
+	printf("Please input a:");
+		scanf("%f",&a);
+	printf("Please input b:");
+		scanf("%f",&b);
+	c=max(a,b);				//c为较大数
+	d=min(a,b);				//d为较小数
+	e=c/d;					//e为余数
 	printf("%f\n", c);
 	printf("%f\n", d);
+	while (e!=0)
+	{
+		if(d>e) 
+		{
+			tmp=e;
+			e=d;
+			d=tmp;
+		}
+		e=d/e;
+
+	}
+	
+	
 	system("pause");
 	return 0;
 }
 
-int max1(int a,int b)
+int max(int a,int b)//此函数返回整型值
 {
 	int c;
 	if (a<b)
@@ -27,9 +43,10 @@ int max1(int a,int b)
 	return c;
 }
 
-int max2(int a,int b)
+int min(int a,int b)
 {
 	int c;
-	a<b?c=b:c=a;
+	a<b?c=a:c=b;
 	return c;
 }
+
